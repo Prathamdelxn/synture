456edde379a67daca8bb3826889da3c9a4c14ec2
seetadelnx <seetasuthar.delnx@gmail.com>
2025-07-04 15:44:37 +0530

    CRUD operation added in user and recruiter

--- src/app/api/recruiter/delete-by-id/[id]/route.js
+++ src/app/api/recruiter/delete-by-id/[id]/route.js
@@ -2,11 +2,13 @@ import dbConnect from "@/utils/db";
 import Recruiter from "@/models/Recruiter";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req) {
+export async function DELETE(request, { params }) {
   await dbConnect();
-  const { id } = await req.json();
+
+  const { id } = params;
 
   const deleted = await Recruiter.findByIdAndDelete(id);
+
   if (!deleted) {
     return NextResponse.json({ error: "Recruiter not found" }, { status: 404 });
   }
--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String },
+    fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     phone: { type: String },
